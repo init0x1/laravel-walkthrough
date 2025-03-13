1d7be59166939c43538dc95f1753bb7d5c695284
init0x1 <abdelrahman.elshaikh7@gmail.com>
2025-03-13 16:29:29 +0200

    props vs attributes

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,4 +1,4 @@
-@props(['active' => false])
+@props(['active' => false]) // the diffrence between props and attributes is that props are passed data to the parent component and attributes are passed to the html element
 
 <a class="{{ $active ? 'bg-gray-900 text-white': 'text-gray-300 hover:bg-gray-700 hover:text-white'}} rounded-md px-3 py-2 text-sm font-medium"
    aria-current="{{ $active ? 'page': 'false' }}"
